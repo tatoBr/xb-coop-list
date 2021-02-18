70d11f899b807a3318b2f64a2f8eb4c9c5a41dad
tato.l.santos@gmail.com <42477514+tatoBr@users.noreply.github.com>
2021-02-18 15:47:52 -0300

    add login and logout controllers

--- controllers/professional.js
+++ controllers/professional.js
@@ -1,4 +1,5 @@
 const { body, validationResult } = require( 'express-validator' );
+const uuid = require( 'uuid' );
 const Services = require( '../services/professional' );
 const services = new Services();
 
@@ -9,29 +10,54 @@ module.exports = {
             adress, actuationfield, skills, experienceLevel,
             portifolioUrl, socialMedias, about, pictureUrl, password
         } = req.body;
-
+        
         let response = await services.save( req.body );        
         if( response.error ) return res.status( 500 ).json({ response: error.message });
         
         res.status( 201 ).json( response );
     },
+    
+    login: async( req, res )=>{
+        const { email, password } = req.body
+        try {
+            let response = await services.loginUser( email, password );
+            res.status( 200 ).json( response );
+            
+        } catch (error) {
+            res.status( 500 ).json({ message: error.message, content: error });
+        }
+    },
+
+    logout: async ( req, res )=>{
+        try {
+            let response = await services.logoutUser();
+            res.status( 200 ).json( response );            
+        } catch (error) {
+            res.status( 500 ).json({ message: error.message, content: error });
+        }
+    },
 
     get: async ( req, res )=>{
         let { id } = req.params;
-        res.status( 200 ).json( req.id );
+        let response = await services.load( id );
+
+        res.status( 200 ).json( response );
     },
 
     getAll: async ( req, res )=>{
-        res.status( 200 ).json( { response: ['Professional', 'List' ]});
+        let response = await services.loadAll();
+        res.status( 200 ).json( response );
     },
 
     patch: async ( req, res )=>{
-        const { id } = req.params;
-        res.status( 202 ).jsos( { response: 'Cliente Atualizado' });
+        let { id }= req.params;
+        let response = await services.update( id, req.body );
+        res.status( 202 ).json( response );
     },
 
     delete: async ( req, res )=>{
         const { id } = req.params;
+        let response = await services.delete( id );
         res.status( 200 ).json({ response: 'Cliente Deletado' });
     }
 }
