27a803ed878415f201a619571819531b02ad1444
tato.l.santos@gmail.com <42477514+tatoBr@users.noreply.github.com>
2021-02-18 13:39:35 -0300

    setup professional route

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//importing Routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const professionalRouter = require( './routes/professional' );
 
 var app = express();
 
@@ -14,7 +16,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//using Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use( '/professionals', professionalRouter );
 
 module.exports = app;
