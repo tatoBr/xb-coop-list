8637bce710444c75b7b502da1029347d7de3ca19
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-02-22 13:17:17 -0300

    Update Professional.js controller

--- controllers/professional.js
+++ controllers/professional.js
@@ -1,28 +1,21 @@
-const { body, validationResult } = require( 'express-validator' );
-const uuid = require( 'uuid' );
+const { URL } = require( 'url' );
 const Services = require( '../services/professional' );
+const { responseMessages : messages } = require( '../utils/variables' );
+
 const services = new Services();
+
 
 module.exports = {
     post: async ( req, res ) => {
-        let { 
-            name, birthdate, email, phoneList,
-            adress, actuationfield, skills, experienceLevel,
-            portifolioUrl, socialMedias, about, pictureUrl, password
-        } = req.body;
-        
         let response = await services.save( req.body );        
         if( response.error ) return res.status( 500 ).json({ response: error.message });
         
         res.status( 201 ).json( response );
     },
     
-    login: async( req, res )=>{
-        const { email, password } = req.body
+    login: async( req, res )=>{        
         try {
-            let response = await services.loginUser( email, password );
-            res.status( 200 ).json( response );
-            
+            res.status(200).send({ message: 'loggedIn', content: { data: req.body.payload, token: req.body.token }})
         } catch (error) {
             res.status( 500 ).json({ message: error.message, content: error });
         }
