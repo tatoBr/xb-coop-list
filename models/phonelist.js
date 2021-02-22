295a888c914b3c8927f41f0ce63fc424e1e58c4b
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-02-22 13:18:08 -0300

    phonelist model now  parse other numbers list to integer.

--- models/phonelist.js
+++ models/phonelist.js
@@ -1,8 +1,8 @@
 module.exports = class PhoneList{
-    constructor( home, work, whatsapp, ...args ){
+    constructor( home, work, whatsapp, others ){
         this.home = home;
         this.work = work;
         this.whatsapp = whatsapp;
-        this.others = [ ...args ];
+        this.others = others.map( p => parseInt( p ) || 0 );
     };
 }
