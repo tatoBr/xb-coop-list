b01ffcea18c7f7624c36d0a6703955056325ecd9
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-02-25 11:05:11 -0300

    redundant files and folders were removed

--- models/phonelist.js
+++
@@ -1,8 +0,0 @@
-module.exports = class PhoneList{
-    constructor( home, work, whatsapp, others ){
-        this.home = home;
-        this.work = work;
-        this.whatsapp = whatsapp;
-        this.others = others.map( p => parseInt( p ) || 0 );
-    };
-}
