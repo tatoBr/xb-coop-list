6e1939f36312cf16175a358c224b96c781c935c3
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-02-25 12:02:53 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = process.env.DATABASE_URL || {
     dialect: 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'admin',
-    database: process.env.DB_NAME || 'xbranding',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'admin',
+    database: 'xbranding',
     define: {
         timestamps: true,
         underscored: true
