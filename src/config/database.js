bc88de132d5c2b9e34720ef322c9c9b08ed34d9f
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-02-26 09:35:26 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
-module.exports = process.env.DATABASE_URL || {
-    host: process.env.DATABASE_HOST || 'localhost',
+module.exports = {
+    host: process.env.DATABASE_HOST || 'ec2-54-90-13-87.compute-1.amazonaws.com',
     port: process.env.DATABASE_PORT || 5432,
-    username: process.env.DATABASE_USER || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'admin',
-    database: process.env.DATABASE || 'xbranding',
+    username: process.env.DATABASE_USER || 'pgzyhiwrjfjbpd',
+    password: process.env.DATABASE_PASSWORD || 'ea69ef26309a63424a3e6fc4114e6b6fe7844a14e99d8124e6c6cdbf53a104ec',
+    database: process.env.DATABASE || 'd9tdeti221lb7q',
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
