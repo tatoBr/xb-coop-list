d1b3cecdd6c485a1983f42ab5178c7251c6c3bdc
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-02-26 15:55:51 -0300

    add a path input validation

--- src/utils/inputValidator.js
+++ src/utils/inputValidator.js
@@ -1,4 +1,5 @@
-const { body, validationResult, query } = require('express-validator');
+const { body, param , query,  validationResult } = require('express-validator');
+
 const { professionalStructure: ps } = require('./variables')
 
 module.exports = {
@@ -58,6 +59,8 @@ module.exports = {
                     .trim()
                     .exists()
                     .withMessage(`The field '${ps.user.cpf}' is required.`)
+                    .isNumeric()
+                    .withMessage('Cep field can only contain numbers.')
                     .isLength(9)
                     .withMessage('cpf must be nine characters long.'),
                 body(ps.user.password)
@@ -161,6 +164,81 @@ module.exports = {
                     .notEmpty()
                     .isAlphanumeric()
             ],
+            patch: [
+                param( ps.user.id )
+                    .trim()
+                    .isUUID()
+                    .withMessage('that\'s not a valid uuid string' ),
+                body([ ps.user.firstname, ps.user.lastname ])
+                    .trim()
+                    .optional({ checkFalsy: true, nullable: true })
+                    .isAlpha('pt-BR', { ignore: ` ._-ñÑ@áéíóúÁÉÍÓÚãÃõÕ0123456789` })
+                    .withMessage(`This field can't contain special characters`)
+                    .isLength({ min: 3, max:48 })
+                    .withMessage(`${ ps.user.firstname } and ${ps.user.lastname} must be a minimum of 3 characters and a maximum of 48` ),
+                body( ps.user.picture )
+                    .trim()
+                    .optional({ checkFalsy: true, nullable: true })
+                    .isBase64()
+                    .withMessage('It\'s not a valid base64 encoded string.'),
+                body( ps.user.birthdate )
+                    .trim()
+                    .optional({ checkFalsy: true, nullable: true })
+                    .isDate({ format: `DD/MM/YYYY` })
+                    .withMessage(`This is not a valid date format`),
+                body( ps.adress.cep )
+                    .trim()
+                    .optional({ checkFalsy: true, nullable: true })
+                    .isNumeric()
+                    .withMessage('Cep field can only contain numbers.')
+                    .isLength(9)
+                    .withMessage('cep must be nine characters long.'),
+                body([ps.adress.street, ps.adress.complement, ps.adress.district, ps.adress.county, ps.adress.state, ps.adress.country])
+                    .trim()
+                    .optional({ checkFalsy: true, nullable: true })
+                    .isAlpha('pt-BR', { ignore: ` ._-ñÑ@áéíóúÁÉÍÓÚãÃõÕ0123456789` })
+                    .withMessage(`This field can't contain special characters`)
+                    .isLength({ min: 1, max: 48 })
+                    .withMessage(`This field must be a minimum of 1 characters and a maximum of 48` ),               
+                body(ps.adress.number)
+                    .trim()
+                    .optional({ checkFalsy: true, nullable: true })
+                    .isNumeric()
+                    .withMessage(`${ps.adress.number} can contain numeric characters only.`)
+                    .isLength({ min: 1, max: 6 })
+                    .withMessage(`${ps.adress.number} range must be 0-999.999.`),
+                body([ ps.phonelist.whatsapp, ps.phonelist.homephone, ps.phonelist.workphone ])
+                    .trim()
+                    .optional({ checkFalsy: true, nullable: true })
+                    .isNumeric()
+                    .withMessage(`this field can contain numeric characters only.`),
+                body([
+                        ps.socialmediaList.instagram,
+                        ps.socialmediaList.facebook,
+                        ps.socialmediaList.youtube,
+                        ps.socialmediaList.linkedin,
+                        ps.socialmediaList.twitter,
+                    ])
+                        .trim()
+                        .optional({ checkFalsy: true, nullable: true })
+                        .isURL()
+                        .withMessage(`Social media links must be a valid URL.`),
+                    body([ps.socialmediaList.clubhouse, ps.socialmediaList.tiktok])
+                        .trim()
+                        .optional({ checkFalsy: true, nullable: true }),                   
+                    body( ps.portifolioUrl )
+                        .trim()
+                        .optional({ checkFalsy: true, nullable: true })
+                        .isURL()
+                        .withMessage(`portifolio link must be a valid URL.`),
+                    body(ps.about)
+                        .trim()
+                        .optional({ checkFalsy: true, nullable: true })
+                        .isAlpha('pt-BR', { ignore: ` -ñÑ@áéíóúÁÉÍÓÚãÃõÕ0123456789,.:` })
+                        .withMessage(`This field can't contain special characters`)
+                        .isLength({ min: 1, max: 2000 })
+                        .withMessage(`This field must have between 1 and 2000 characters`),
+                    ],
             selectAll: [
                 query(['page', 'limit' ])
                     .trim()
