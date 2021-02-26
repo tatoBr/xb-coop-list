2350e52c8da4778e0bb642fce2cdb4f62be424df
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-02-26 09:21:59 -0300

    Update authorization.js

--- src/utils/authorization.js
+++ src/utils/authorization.js
@@ -2,18 +2,22 @@ const jwt = require( 'jsonwebtoken' );
 const { privateKey } = require('./variables' );
 const { professionalStructure: ps, userAccessLevel } = require( './variables')
 
-const generateToken = ( payload )=>{
-    return jwt.sign( payload, process.env.PRIVATE_KEY || privateKey, { expiresIn : '1h' })
-}
+const generateAccessToken = ( payload )=>{
+    return jwt.sign( payload, process.env.ACCESS_TOKEN_SECRET || 'ACCESS SECRET', { expiresIn : '30m' });
+};
 
-const professional = ( req, res, next )=>{
+const generateRefreshToken = ( payload ) => {
+    return jwt.sign( payload, process.env.REFRESH_TOKEN_SECRET || 'REFRESH SECRET', { expiresIn: '7d' });
+};
+
+const checkProfessionalPassport = ( req, res, next )=>{    
     const id = req.params[ps.user.id];
     const token = req.header('Authorization')?.split(' ')[1];
 
     if( !token ) return res.status( 401 ).json({ message: 'You don\'t have permission to access this resource' });
 
     try {
-        const decoded = jwt.verify( token, process.env.PRIVATE_KEY || privateKey );      
+        const decoded = jwt.verify( token, process.env.ACCESS_TOKEN_SECRET || 'ACCESS SECRET' );      
         if(( id !== decoded[ps.user.id] ) || ( decoded[ps.user.accessLevel] !== userAccessLevel.professional ))
             return res.status( 401 ).json({ message: 'You don\'t have permission to access this resource' });
 
@@ -30,6 +34,7 @@ const professional = ( req, res, next )=>{
 const checkAccessLevel = ()=>null;
 
 module.exports = {
-    generateToken,
-    professional
+    generateAccessToken,
+    generateRefreshToken,   
+    checkProfessionalPassport
 }
