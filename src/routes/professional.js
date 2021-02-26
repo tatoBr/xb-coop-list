212730ee52b3ef724522dca98f230aa83afeac2a
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-02-26 12:29:06 -0300

    add input validator to get all endpoint

--- src/routes/professional.js
+++ src/routes/professional.js
@@ -1,17 +1,24 @@
 const { Router } = require( 'express' );
+const Professional = require( '../models/professional' )
+const User = require( '../models/user' );
 const controller = require( '../controllers/professional' );
+const { professionalStructure : ps } = require('../utils/variables')
 const { checkProfessionalPassport  } = require( '../utils/authorization' );
-const inputValidator = require( '../utils/inputValidator');
+const { professional: { chains : validationChains }, checkResults} = require( '../utils/inputValidator');
+const paginate = require( '../utils/paginate' );
 
 const router = Router();
 
 router.post( '/',
-    inputValidator.professional.chains.insert,
-    inputValidator.checkResults,
+    validationChains.insert,
+    checkResults,    
     controller.post
 );
 
-router.get( '/', controller.getAll );
+router.get( '/',
+    validationChains.selectAll,
+    checkResults,
+    controller.getAll );
 
 router.get( '/:id', controller.get );
 
