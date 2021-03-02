9fbc1d20d50367f20caca5bc4e6211b356f9d996
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-03-02 15:08:04 -0300

    add 'professional status'  and 'user deleted' variable

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -64,6 +64,7 @@ module.exports = {
         experienceLevel: 'experienceLevel',
         portifolioUrl: 'portifolioUrl',
         about: 'about',
+        status: 'status',
                
     },
     responseMessages: {
@@ -71,13 +72,14 @@ module.exports = {
         USER_LOADED: 'USER LOADED SUCCESSFULLY',
         USER_UPDATED: 'USER WAS UPDATED SUCCESSFULLY',
         USERS_LOADED: 'USERS LOADED SUCCESSFULLY',
+        USER_DELETED: 'USER WAS DELETED FROM THE DATABASE',
         USER_LOGGED_OUT: 'USER LOGGED OUT FROM THE SYSTEM',
         USER_ALREADY_EXIST: 'USER ALREADY EXIST IN THE DATABASE',
         USER_NOT_FOUND: 'USER DON\'T EXIST IN THE DATABASE',
         PASSWORD_MISMATCH: 'WRONG PASSWORD',
         USER_AUTHENTICATED: 'USER AUTHENTICATED SUCCESSFULLY.',
         USER_IN_WAIT_TIME: 'TOO MANY FAILED LOGIN TRIES. WAIT FOR A FEW MINUTES AND TRY AGAIN.',       
-        UNAUTHORIZED: 'YOU DON\'T HAVE PERMISSION TO ACCESS THIS RESOURCE',
+        UNAUTHORIZED: 'MISSING OR INVALID AUTHORIZATION HEADER.',
         ACCESS_TOKEN_GENERATED:'SUCCESSFULLY GENERATED AN ACCESS TOKEN'      
     },
     privateKey: "2812a7a341e863e76dad78e0ea8be22e",
@@ -85,5 +87,10 @@ module.exports = {
         owner: 'own',
         admin: 'adm',
         professional:'prf'
+    },
+    professionalStatus: {
+            INACTIVE : 'INACTIVE',
+            ACTIVE: 'ACTIVE',
+            IN_ANALYSIS: 'IN ANALYSIS'
     }
 }
