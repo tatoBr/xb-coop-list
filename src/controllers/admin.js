723e21985ae38fc03dad16c54d45aab41596a8a5
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-03-05 07:36:13 -0300

    add controllers to manage professionals as admin

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -1,5 +1,7 @@
 const Services = require('../services/admin');
 const userServices = require( '../services/user' );
+const ProfessionalServices = require( '../services/professional' );
+const profServices = new ProfessionalServices();
 const { responseMessages : rmsgs } = require( '../utils/variables' );
 
 const adminServices = new Services();
@@ -80,6 +82,95 @@ module.exports = {
             } 
 
             return res.status(404).json({ authentication })      
+        } catch (error) {
+            return res.status( 500 ).json({ message: error.message, content: error });
+        }
+    },
+
+    readAllProfessionals: async( req, res )=>{        
+        try {
+            let professionals = await profServices.loadAll( req.query )
+            switch ( professionals.message ) {
+                case rmsgs.USERS_LOADED:
+                    res.status( 200 );
+                    break;
+                case rmsgs.NO_USER_TO_LOAD:
+                    res.status( 204 );
+                    break;
+                default:
+                    res.status( 500 );
+                    break;
+            }
+            return res.json( professionals );                     
+        } catch (error) {
+            return res.status( 500 ).json({ message: error.message, content: error });
+        }
+    },
+
+    readProfessionalById: async( req, res )=>{
+        try {            
+            let { profId } = req.params;
+            let professional = await profServices.loadById( profId, req.query );
+            
+            switch (professional.message) {
+                case rmsgs.USER_LOADED:
+                    res.status( 200 );
+                    break;
+                    
+                case rmsgs.USER_NOT_FOUND:
+                    res.status( 204 );
+                    break;
+                        
+                default:
+                    res.status( 500 );
+                    break;
+            }
+            res.json( professional );
+        } catch (error) {
+            return res.status( 500 ).json({ message: error.message, content: error });
+        }
+    },
+
+    updateProfessional: async( req, res )=>{
+        try {
+            let { profId } = req.params;
+            let professional = await profServices.update(profId, req.body );        
+            switch (professional.message ) {
+                case rmsgs.USER_NOT_FOUND:
+                    res.status( 400 )                
+                    break;
+    
+                case rmsgs.USER_UPDATED:
+                    res.status( 200 )                
+                    break;
+            
+                default:
+                    break;
+            }
+            res.json( professional );            
+        } catch (error) {
+            return res.status( 500 ).json({ message: error.message, content: error });
+        }
+    },
+
+    deleteProfessional: async( req, res )=>{
+        try {  
+            let { profId } = req.params;          
+            let professional = await profServices.delete( profId );
+            switch (professional.message ) {
+                case rmsgs.USER_NOT_FOUND:
+                    res.status( 400 )                
+                    break;
+    
+                case rmsgs.USER_DELETED:
+                    res.status( 200 )                
+                    break;
+            
+                default:
+                    res.status( 500 );
+                    break;
+            }
+            res.json( professional );
         } catch (error) {
             return res.status( 500 ).json({ message: error.message, content: error });
         }
