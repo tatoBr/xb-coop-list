f33747ee40dff46f77c8294b6a796b6987e1a4f7
Renato Lima Santos <42477514+tatoBr@users.noreply.github.com>
2021-03-05 07:33:03 -0300

    add input validation to the logout route

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,13 +4,18 @@ const router = express.Router();
 const controller = require( '../controllers/user' );
 const { user: { chains: validationChains }, checkResults } = require( '../utils/inputValidator');
 
-router.get('/login',
+router.patch('/login',
   validationChains.authentication,
   checkResults,
   controller.authenticate
 );
 
-router.get( '/logout/:id', controller.logout );
+router.patch(
+  '/logout/:id',
+  validationChains.logout,
+  checkResults,
+  controller.logout
+);
 router.get('/acesstoken/:id', controller.getAccessToken );
 
 module.exports = router;
